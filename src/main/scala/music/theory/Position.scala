dfe847b3426061e627b46ceea0ff28a818f7b4b7
Francois Cabrol <francois.cabrol@live.ca>
2017-05-07 21:21:31 -0400

    Move the Signature class

--- src/main/scala/music/theory/Position.scala
+++ src/main/scala/music/theory/Position.scala
@@ -1,19 +1,6 @@
 package music.theory
 
-import music.{Rational, RationalNumber}
-
-// Example Signature(4, 4)
-case class Signature(timeUnitsPerBar:Int, timeUnit: Int) {
-  require(Array(1, 2, 4, 8).contains(timeUnit), "The time unit should be 1, 2, 4 or 8")
-  def timeUnitDuration: Duration = timeUnit match {
-    case 1 => Duration.WHOLE_NOTE
-    case 2 => Duration.HALF_NOTE
-    case 4 => Duration.QUARTER_NOTE
-    case 8 => Duration.EIGHT_NOTE
-  }
-
-  def quarterNotesPerBar: Int = ((timeUnitDuration / Duration.QUARTER_NOTE).toFloat * timeUnitsPerBar).toInt
-}
+import music.RationalNumber
 
 case class Position private(initialNumer: Int, initialDenom: Int = 1) extends RationalNumber[Position](initialNumer, initialDenom) with Ordered[Position] {
 
